f679716e450db64ab9812950230e34f39596b19f
subirjolly <subirjolly>
2020-11-30 18:27:21 -0500

    minor update.

--- lighthouse/login.js
+++ lighthouse/login.js
@@ -14,87 +14,87 @@ if (!args.username || !args.password || !args.port) {
     return;
 }
 
-try {
-    (async() => {
+(async() => {
+    let loginURL = `http://kubernetes.docker.internal`;
+    let logoutURL = `http://kubernetes.docker.internal/logout`;
 
-        const loginURL = `http://kubernetes.docker.internal:${args.port}`;
-        const logoutURL = `http://kubernetes.docker.internal:${args.port}/logout`;
-    
-        const opts = {
-            //chromeFlags: ['--headless'],
-            logLevel: 'info',
-            output: 'json',
-            disableDeviceEmulation: true,
-            defaultViewport: {
-                width: 1200,
-                height: 900
-            },
-            chromeFlags: ['--disable-mobile-emulation']
-        };
-    
+    if (args.port !== 80) {
+        loginURL = `http://kubernetes.docker.internal:${args.port}`;
+        logoutURL = `http://kubernetes.docker.internal:${args.port}/logout`;
+    }
+
+    const opts = {
+        // chromeFlags: ['--headless'],
+        logLevel: 'info',
+        output: 'json',
+        disableDeviceEmulation: true,
+        defaultViewport: {
+            width: 1200,
+            height: 900
+        },
+        chromeFlags: ['--disable-mobile-emulation']
+    };
+
     // Launch chrome using chrome-launcher
-        const chrome = await chromeLauncher.launch(opts);
-        opts.port = chrome.port;
-    
-    // Connect to chrome using puppeteer.connect().
-        const resp = await util.promisify(request)(`http://localhost:${opts.port}/json/version`);
-        const {webSocketDebuggerUrl} = JSON.parse(resp.body);
-        const browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
-    
-    //Puppeteer
-        const page = (await browser.pages())[0];
-        await page.setViewport({ width: 1200, height: 900});
-        await page.goto(loginURL, {waitUntil: 'networkidle0'}).catch(async e => {
-            await browser.disconnect();
-            await chrome.kill();
+    const chrome = await chromeLauncher.launch(opts);
+    opts.port = chrome.port;
 
-            return void(0);
-        });
-        // await page.waitForNavigation({ waitUntil: 'networkidle0' });
-        await page.type('[id="login"]', args.username);
-        await page.type('[id="password"]', args.password);
-        await page.evaluate(() => {
-            document.querySelector('[id="submit-login"]').click();
-        });
-    
-        await page.waitForNavigation({ waitUntil: 'networkidle2' });
-        await page.click('body > div.dex-container > div > div:nth-child(5) > div:nth-child(1) > form > button');
-        await page.waitForNavigation({waitUntil: 'networkidle2'});
-        await page.waitForSelector('#cf-app-wrapper', {
-            visible: true,
-          });
-    
-        console.log('PAGE URL: ', page.url(), opts, config);
-    
-        // Run Lighthouse.
-        const report = await lighthouse(page.url(), opts, config).then(results => {
-            return results;
-        });
-    
-        const html = reportGenerator.generateReport(report.lhr, 'html');
-        const json = reportGenerator.generateReport(report.lhr, 'json');
-    
-        await page.goto(logoutURL, {waitUntil: 'networkidle2'});
-    
+    // Connect to chrome using puppeteer.connect().
+    const resp = await util.promisify(request)(`http://localhost:${opts.port}/json/version`);
+    const {webSocketDebuggerUrl} = JSON.parse(resp.body);
+    const browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
+
+    //Puppeteer
+    const page = (await browser.pages())[0];
+    await page.setViewport({ width: 1200, height: 900});
+    await page.goto(loginURL, {waitUntil: 'networkidle0'}).catch(async e => {
         await browser.disconnect();
         await chrome.kill();
-    
-    
-        //Write report html to the file
-        fs.writeFile('LighthouseReport.html', html, (err) => {
-            if (err) {
-                console.error(err);
-            }
+
+        return void(0);
+    });
+
+    // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    await page.type('[id="login"]', args.username);
+    await page.type('[id="password"]', args.password);
+    await page.evaluate(() => {
+        document.querySelector('[id="submit-login"]').click();
+    });
+
+    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    await page.click('body > div.dex-container > div > div:nth-child(5) > div:nth-child(1) > form > button');
+    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForSelector('#cf-app-wrapper', {
+        visible: true,
         });
-    
-        //Write report json to the file
-        fs.writeFile('LighthouseReport.json', json, (err) => {
-            if (err) {
-                console.error(err);
-            }
-        });
-    
-    })();
-} catch(e) {
-    console.log(e);
-}
+
+    console.log('PAGE URL: ', page.url(), opts, config);
+
+    // Run Lighthouse.
+    const report = await lighthouse(page.url(), opts, config).then(results => {
+        return results;
+    });
+
+    const html = reportGenerator.generateReport(report.lhr, 'html');
+    const json = reportGenerator.generateReport(report.lhr, 'json');
+
+    await page.goto(logoutURL, {waitUntil: 'networkidle2'});
+
+    await browser.disconnect();
+    await chrome.kill();
+
+
+    //Write report html to the file
+    fs.writeFile('LighthouseReport.html', html, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+
+    //Write report json to the file
+    fs.writeFile('LighthouseReport.json', json, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+})();
